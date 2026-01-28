d850d33ad175d228f4166df63a06fd1829ff92e5
danielepalomba <palombad46@gmail.com>
2026-01-28 18:52:59 +0100

    fixed memory leak in features.py

--- model/features.py
+++ model/features.py
@@ -7,12 +7,29 @@ class FeatureExtractor:
         self.global_window = deque()
         # Maps MAC -> { 'last_ts': int, 'count_window': deque() }
         self.device_states = defaultdict(lambda: {'last_ts': 0, 'count_window': deque()})
+        self.packet_count = 0
+
+    def cleanup(self, current_timestamp, timeout=300):
+        """
+        Removes devices that haven't been seen for 'timeout' seconds.
+        """
+        to_remove = []
+        for mac, state in self.device_states.items():
+            if current_timestamp - state['last_ts'] > timeout:
+                to_remove.append(mac)
+        
+        for mac in to_remove:
+            del self.device_states[mac]
 
     def process(self, mac, timestamp):
         """
         Calculates features for a single packet.
         Returns: np.array([[delta, local_freq, global_freq]])
         """
+        self.packet_count += 1
+        if self.packet_count % 1000 == 0:
+            self.cleanup(timestamp)
+
         # Global Frequency
         # Remove old timestamps (< timestamp - 60)
         while self.global_window and self.global_window[0] <= timestamp - 60:
